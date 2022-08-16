b0db692a10fe66dab38c963e7ebdcf00971bec08
gaokui <gaokui@cestc.cn>
2022-08-16 18:06:38 +0800

    精简化修改

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,8 +1,6 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: 'one',link: '/one/',},
-  { text: 'two',link: '/two/',},
-  { text: 'three', link: '/three/' },
+  { text: 'fighting',link: '/fighting/',},
   { text: '留言板', link: '/message-board/' },
 ]
