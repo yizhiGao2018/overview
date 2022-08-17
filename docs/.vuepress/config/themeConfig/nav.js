e86418a96b9dbf5d58ff957d9d3c2f167223e8c4
gaokui <gaokui@cestc.cn>
2022-08-17 12:33:20 +0800

    简化测试

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,8 +1,8 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: 'one',link: '/one/',},
-  { text: 'two',link: '/two/',},
-  { text: 'three', link: '/three/' },
+  { text: '生平历史',link: '/resume/',},
+  { text: '知识积累',link: '/knowledge/',},
+  { text: '消息分享', link: '/message-share/' },
   { text: '留言板', link: '/message-board/' },
 ]
